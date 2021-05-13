62ee9dc35950228149ada7e9ddb21d9722bf2966
austinandrade <austinmandrade@gmail.com>
2021-05-13 18:04:13 -0400

    Add welcome index route
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  
   resources :users
   post '/users/:id/follow', to: "users#follow", as: "follow_user"
   post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"

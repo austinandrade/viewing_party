6d3e93e7322f6fbaf7b86f885be7cc6495bd0ee3
austinandrade <austinmandrade@gmail.com>
2021-05-12 20:41:21 -0400

    Create relationships and validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
+  has_many :followees, through: :followed_users
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
+  has_many :followers, through: :following_users
+
+  has_many :parties
+  has_many :viewing_party_guests
 end

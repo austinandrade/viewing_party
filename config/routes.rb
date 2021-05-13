23b9f3ec7160b3ca10eecd87c7830a6748834dd5
austinandrade <austinmandrade@gmail.com>
2021-05-13 19:15:54 -0400

    Add handrolled login to sessions_controller actions
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
-  
+
   resources :users
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   post '/users/:id/follow', to: "users#follow", as: "follow_user"
   post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
 end

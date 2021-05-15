c6a76e3c09b6644327e65a7a5f82b7cf0e1a3f8c
austinandrade <austinmandrade@gmail.com>
2021-05-15 12:16:00 -0400

    Remove needless migration

--- db/migrate/20210514013126_change_column.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumn < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :friendships, :user_id, :follower_id
-    rename_column :friendships, :friend_id, :followee_id
-  end
-end

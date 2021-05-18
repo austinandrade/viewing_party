6b9aa5760c0371ac19b4512d9ab987b237810784
austinandrade <austinmandrade@gmail.com>
2021-05-18 17:40:27 -0400

    Add all endpoint hits for displaying a movies attributes and reviews
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -4,7 +4,27 @@ class MoviesService
     def top_rated_movies
       prepare_json('/3/movie/top_rated')
     end
-    
+
+    def search_db_my_movie(movie_search)
+      response = conn.get("3/search/movie?query=#{movie_search}")
+      parse_data(response)
+    end
+
+    def search_db_by_movie_id(movie_id)
+      response = conn.get("3/movie/#{movie_id}")
+      parse_data(response)
+    end
+
+    def search_movie_cast_db(movie_id)
+      response = conn.get("3/movie/#{movie_id}/credits")
+      parse_data(response)
+    end
+
+    def search_movie_review_db(movie_id)
+      response = conn.get("3/movie/#{movie_id}/reviews")
+      parse_data(response)
+    end
+
     private
 
     def conn
@@ -14,7 +34,11 @@ class MoviesService
     end
 
     def parse_data(response)
-      JSON.parse(response.body, symbolize_names: true)
+      if response.body.empty?
+        []
+      else
+        JSON.parse(response.body, symbolize_names: true)
+      end
     end
 
     def prepare_json(path)

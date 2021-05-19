205e3c5522bdab1b219cd491c5bbedb0260b102a
austinandrade <austinmandrade@gmail.com>
2021-05-19 13:38:13 -0400

    Remove movie references for passing tests
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 describe 'user login' do
   before :each do
-    @movie_1 = Movie.create(title: "King Kong")
-    @movie_2 = Movie.create(title: "Castaway")
-
     @user_1 = User.create!(email: 'bobo@gmail.com', password: 'smokeweedeveryday')
     @user_2 = User.create!(email: 'bobo1@gmail.com', password: 'smokeweedeveryday')
     @user_3 = User.create!(email: 'bobo2@gmail.com', password: 'smokeweedeveryday')
@@ -14,13 +11,13 @@ describe 'user login' do
     @friendship_2 = Friendship.create(follower_id: @user_3.id, followee_id: @user_2.id)
     @friendship_3 = Friendship.create(follower_id: @user_4.id, followee_id: @user_2.id)
 
-    @party_1 = Party.create(user_id: @user_2.id, movie_id: @movie_1.id, duration: 120, date: '9999-12-31', start_time: "13:00:00")
+    @party_1 = Party.create(user_id: @user_2.id, movie_title: "Pink", duration: 120, date: '9999-12-31', start_time: "13:00:00")
 
     @viewing_party_guests_1 = ViewingPartyGuest.create(user_id: @user_1.id, party_id: @party_1.id)
     @viewing_party_guests_2 = ViewingPartyGuest.create(user_id: @user_3.id, party_id: @party_1.id)
     @viewing_party_guests_3 = ViewingPartyGuest.create(user_id: @user_4.id, party_id: @party_1.id)
 
-    @party_2 = Party.create(user_id: @user_1.id, movie_id: @movie_2.id, duration: 120, date: '2011-10-25', start_time: "15:00:00")
+    @party_2 = Party.create(user_id: @user_1.id, movie_title: "Wall-E", duration: 120, date: '2011-10-25', start_time: "15:00:00")
     @viewing_party_guests_4 = ViewingPartyGuest.create(user_id: @user_2.id, party_id: @party_2.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
@@ -105,25 +102,24 @@ describe 'user login' do
         expect(page).to have_content("#{@user_3.email} is invited")
         expect(page).to have_content("#{@user_4.email} is invited")
 
-        expect(page).to have_content(@movie_1.title)
+        expect(page).to have_content(@party_1.movie_title)
         expect(page).to have_content("12/31/9999")
         expect(page).to have_content("at 01:00PM")
         expect(page).to have_content("#{@party_1.duration} minutes")
       end
 
       within("#invited_to-#{@party_2.id}") do
-        expect(page).to have_content(@movie_2.title)
+        expect(page).to have_content(@party_2.movie_title)
         expect(page).to have_content("Invited by Host: #{@user_1.email}")
 
         expect(page).to have_content("#{@user_2.email} is invited")
         page.html.should include("<b> bobo1@gmail.com is invited </b>")
 
-        expect(page).to have_content(@movie_2.title)
+        expect(page).to have_content(@party_2.movie_title)
         expect(page).to have_content("10/25/2011")
 
         expect(page).to have_content("at 03:00PM")
         expect(page).to have_content("#{@party_2.duration} minutes")
-        # save_and_open_page
       end
     end
   end
